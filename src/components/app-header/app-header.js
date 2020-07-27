fdcfc70e5bc509a124a7ae865306704d79006020
Silvia <gabrielyan.silvia89@gmail.com>
2020-07-27 17:05:15 +0300

    new

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppHeader = () => {
+	return (
+		<div className="app-header d-flex">
+			<h1>Silvia Gabrielyan</h1>
+			<h2>5 posts, of them liked 3</h2>
+			</div>
+		)
+}
+
+export default AppHeader;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchPanel = () => {
+	return (
+		<input
+		className="from-control search-input"
+		type="text"
+		placeholder="search by records"
+		/>
+		)
+}
+
+export default SearchPanel;
