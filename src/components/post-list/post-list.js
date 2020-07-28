4c474c4a73587ea424c8d8414a51ced46691ac9d
Silvia <gabrielyan.silvia89@gmail.com>
2020-07-29 00:42:43 +0300

    new work

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 import PostListItem from '../post-list-item';
+import './post-list.css'
 
-const PostList = () => {
+const PostList = ({posts}) => {
+
+	const elements = posts.map((item) => {
+		const {id, ...itemProps} = item;
+		return (
+			<li key={item.id} className='post-list-item'>
+			<PostListItem {...itemProps}/>
+			</li>
+		)
+	})
+
+
+
 	return (
 		<ul className="app-list list-group">
-			<PostListItem/>
-			<PostListItem/>
-			<PostListItem/>
+			{elements}
 		</ul>
 	)
 }
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './search-panel.css'
+
 const SearchPanel = () => {
 	return (
 		<input
@@ -7,7 +9,7 @@ const SearchPanel = () => {
 		type="text"
 		placeholder="search by records"
 		/>
-		)
+	)
 }
 
 export default SearchPanel;
