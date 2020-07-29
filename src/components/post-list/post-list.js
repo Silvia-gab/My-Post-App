25dddf27069f957933713e55f1145e58a40c8ca8
Silviagabrielyan.silvia89@gmail.com <gabrielyan.silvia89@gmail.com>
2020-07-29 15:33:03 +0300

    bootstrap

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,34 +1,28 @@
 import React from 'react';
 
 import PostListItem from '../post-list-item';
-import './post-list.css'
+import {ListGroup} from 'reactstrap';
+import './post-list.css';
+
 
 const PostList = ({posts}) => {
 
 	const elements = posts.map( (item) => {
-		if ( typeof item === 'object' && isEmpty(item) ) {
+		
 		const {id, ...itemProps} = item;
 		return (
 			<li key = {id} className='list-group-item'>
 			<PostListItem {...itemProps}/>
 			</li>
 		 )
-	   }
-	})
-
-	function isEmpty(obj) {
-		for(let key in obj)
-		{
-			return true;
-		}
-		return false;
-	}
+	   
+	});
 
 
 	return (
-		<ul className="app-list list-group">
+		<ListGroup className="app-list">
 			{elements}
-		</ul>
+		</ListGroup>
 	)
 }
 
